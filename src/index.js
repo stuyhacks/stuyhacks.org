7bf60ae5ab7856250c5b70cf606c2b000b2f284a
Jason Kao <jasonkao85@gmail.com>
2018-02-04 12:01:02 -0500

    index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
+import "babel-polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { RoutingApp } from "./modules";
+import { AppContainer } from "react-hot-loader";
+import injectTapEventPlugin from "react-tap-event-plugin";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// for onClick events with MUI/React
+try {
+  injectTapEventPlugin();
+} catch (err) {
+  /* hot reloading, no issue  */
+}
+
+ReactDOM.render(
+  <AppContainer>
+    <RoutingApp />
+  </AppContainer>,
+  document.getElementById("root"),
+);
 registerServiceWorker();
